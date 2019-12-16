bfbbeb314978b4d843b00c7cd2651da9efa8f606
Lee Bardon <leerbardon@gmail.com>
2019-12-16 15:25:24 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,21 +4,25 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  subtract = num1 - num2 
+  return subtract
 end
 
 def division(num1, num2)
-
+  divide = num1 / num2
+  return divide
 end
 
 def multiplication(num1, num2)
-
+  multiply = num1 * num2
+  return multiply
 end
 
 def modulo(num1, num2)
-
+  mod = num1 % num2
+  return mod
 end
 
 def square_root(num)
-
+  return square_root(num)
 end
